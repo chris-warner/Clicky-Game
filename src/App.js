5eafb7108fa81b0b6beeb723187a9bc417a21491
Chris Warner <christopher.warner92@gmail.com>
2019-06-04 10:05:34 -0400

    fixed blank space at top of page by setting margin auto in title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Nav from './components/nav.js';
+import Nav from './components/Nav.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-            <Nav />
+      <Nav />
       <header className="App-header">
       </header>
     </div>
