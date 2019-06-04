663ca88ce4bdaf3e165112a4e97e4c6ada661d90
Chris Warner <christopher.warner92@gmail.com>
2019-06-04 09:48:46 -0400

    created and implimented  nav component

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav style={navbar}> </nav>
+    );
+}
+
+const navbar = {
+    height:'125px',
+    backgroundColor: '#506FE5'
+
+}
+
+export default Nav;
