0908723e957980bab33b911a4e9d7fd9bd65ef65
Chris Warner <christopher.warner92@gmail.com>
2019-06-09 01:11:17 -0400

    converted ImageTile to class based component, now keeps track of clicked state

--- src/components/ImageTile.jsx
+++ src/components/ImageTile.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Image} from 'react-bootstrap';
+import React from 'react';
+import { Image } from 'react-bootstrap';
 import image1 from '../img/galaxy1.jpg';
 import image2 from '../img/galaxy2.jpg';
 import image3 from '../img/galaxy3.jpeg';
@@ -13,26 +13,35 @@ import image10 from '../img/galaxy10.jpg';
 import image11 from '../img/galaxy11.jpg';
 import image12 from '../img/galaxy12.jpg';
 
-function ImageTile() {
-    const imageArray = [image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12];
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+class ImageTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.clicked = 0;
     }
-    console.log(getRandomInt(12));
-    const [Clicked, setClicked] = useState(0);
-
-    return (
-        <div style={tileStyle}>
-            <p>{getRandomInt(12)}</p>
-            <Image style={imageStyle} src={[imageArray[getRandomInt(12)]]}></Image>
-        </div>
-    );
+    
+    render() {
+        const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        console.log(getRandomInt(12));
+        return (
+            <div style={tileStyle}>
+                <p>{getRandomInt(12)}</p>
+                <Image style={imageStyle} src={[imageArray[getRandomInt(12)]]}></Image>
+            </div>
+        );
+    }
 }
-
+const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        console.log(getRandomInt(12));
 const imageStyle = {
-    height:'150px',
-    width:'150px',
-    color:'white'
+    height: '150px',
+    width: '150px',
+    color: 'white'
 }
 
 const tileStyle = {
@@ -42,7 +51,7 @@ const tileStyle = {
     borderColor: 'black',
     borderWidth: '2px',
     borderStyle: 'inline',
-    color:'white'
+    color: 'white'
 }
 
 export default ImageTile;
