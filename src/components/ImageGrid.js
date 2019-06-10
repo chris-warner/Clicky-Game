b771a5f678348349878009efc845cff9016171f8
Chris Warner <christopher.warner92@gmail.com>
2019-06-10 15:27:05 -0400

    removed ImageTile component as it was causing to many issues. Loaded images directly in ImageGrid. ImageArray is shuffled and loadee into images on load

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,34 +1,72 @@
-import React from "react";
+import React, {useState,useEffect} from 'react'
 import {Row,Col,Container} from 'react-bootstrap';
-import ImageTile from '../components/ImageTile';
+import ImageTile from './ImageTile'
+import image1 from '../img/galaxy1.jpg';
+import image2 from '../img/galaxy2.jpg';
+import image3 from '../img/galaxy3.jpeg';
+import image4 from '../img/galaxy4.jpeg';
+import image5 from '../img/galaxy5.jpeg';
+import image6 from '../img/galaxy6.jpeg';
+import image7 from '../img/galaxy7.png';
+import image8 from '../img/galaxy8.jpg';
+import image9 from '../img/galaxy9.jpg';
+import image10 from '../img/galaxy10.jpg';
+import image11 from '../img/galaxy11.jpg';
+import image12 from '../img/galaxy12.jpg';
+import {Image} from 'react-bootstrap';
 
-class ImageGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = 0;
-    }
-    render() {
+function ImageGrid () {
+
+      const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+      let shuffledImageArray = shuffle(imageArray);
+
+      function shuffleImages() {
+        shuffledImageArray = shuffle(imageArray);
+      }
+     
+      function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      }
         return (
             <Container>
             <Row style={rowStyle} className="justify-content-md-center">
-              <Col style={colStyle}>1 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>2 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>3 of 3 <ImageTile /></Col>
+              <Col style={colStyle}>1 of 3 <Image style={imgStyle} src={shuffledImageArray[0]}/> </Col>
+              <Col style={colStyle}>2 of 3 <Image style={imgStyle} src={shuffledImageArray[1]}/> </Col>
+              <Col style={colStyle}>3 of 3 <Image style={imgStyle} src={shuffledImageArray[2]}/> </Col>
             </Row>
             <Row style={rowStyle}>
-              <Col style={colStyle}>1 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>2 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>3 of 3 <ImageTile /></Col>
+              <Col style={colStyle}>1 of 3 <Image style={imgStyle} src={shuffledImageArray[3]}/></Col>
+              <Col style={colStyle}>2 of 3 <Image style={imgStyle} src={shuffledImageArray[4]}/></Col>
+              <Col style={colStyle}>3 of 3 <Image style={imgStyle} src={shuffledImageArray[5]}/></Col>
             </Row>
             <Row style={rowStyle}>
-              <Col style={colStyle}>1 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>2 of 3 <ImageTile /></Col>
-              <Col style={colStyle}>3 of 3 <ImageTile /></Col>
+              <Col style={colStyle}>1 of 3 <Image style={imgStyle} src={shuffledImageArray[6]}/></Col>
+              <Col style={colStyle}>2 of 3 <Image style={imgStyle} src={shuffledImageArray[7]}/></Col>
+              <Col style={colStyle}>3 of 3 <Image style={imgStyle} src={shuffledImageArray[8]}/></Col>
             </Row>
           </Container>
         );
     }
-}
+
+    const imgStyle ={
+      height: '100px',
+      width:'100px'
+    }
 
 const colStyle = {
     height: '300px',
--- src/components/ImageTile.jsx
+++ src/components/ImageTile.jsx
@@ -1,57 +1,21 @@
-import React from 'react';
-import { Image } from 'react-bootstrap';
-import image1 from '../img/galaxy1.jpg';
-import image2 from '../img/galaxy2.jpg';
-import image3 from '../img/galaxy3.jpeg';
-import image4 from '../img/galaxy4.jpeg';
-import image5 from '../img/galaxy5.jpeg';
-import image6 from '../img/galaxy6.jpeg';
-import image7 from '../img/galaxy7.png';
-import image8 from '../img/galaxy8.jpg';
-import image9 from '../img/galaxy9.jpg';
-import image10 from '../img/galaxy10.jpg';
-import image11 from '../img/galaxy11.jpg';
-import image12 from '../img/galaxy12.jpg';
+import React, {useState,useEffect} from 'react';
+import {Image} from 'react-bootstrap';
 
-class ImageTile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.clicked = 0;
-    }
-    
-    render() {
-        const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * Math.floor(max));
-        }
-        console.log(getRandomInt(12));
-        return (
-            <div style={tileStyle}>
-                <p>{getRandomInt(12)}</p>
-                <Image style={imageStyle} src={[imageArray[getRandomInt(12)]]}></Image>
-            </div>
-        );
-    }
-}
-const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * Math.floor(max));
-        }
-        console.log(getRandomInt(12));
-const imageStyle = {
-    height: '150px',
-    width: '150px',
-    color: 'white'
-}
+function ImageTile () {
+    const [imageSrc, setImageSrc] = useState("");
+    const [count, setCount] = useState(0);
 
-const tileStyle = {
-    height: '150px',
-    width: '150px',
-    backgroundColor: 'Black',
-    borderColor: 'black',
-    borderWidth: '2px',
-    borderStyle: 'inline',
-    color: 'white'
+    const setSrc = (props) => {
+    setImageSrc(props);
+    return
+    }
+
+    return(
+        <div>
+        <h1 onClick={() => setCount(count + 1)} >{count}</h1>
+        <Image src={imageSrc} />
+        </div>
+    );
 }
 
 export default ImageTile;
